ba39d26af9e4ea3e191c75fcf24a9369bc2ca386
peyton11 <peyton.gardner03@gmail.com>
2023-05-09 14:37:10 -0400

    add player count

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 // Function Prototypes:
 void printBoard(char[]);
+int players();
 
 int main()
 {
@@ -21,4 +22,12 @@ void printBoard(char board[])
 	std::cout << board[3] << " | " << board[4] << " | " << board[5] << std::endl;
 	std::cout << "----------" << std::endl;
 	std::cout << board[6] << " | " << board[7]<< " | " << board[8] << std::endl;
+}
+
+int players()
+{
+	int players;
+	std::cout << "How many players?\n1 Player[1]\n2 Player[2]\n";
+	std::cin >> players;
+	return players;
 }
