a0cbbe99447739b06f39ef95ba65601319503690
peyton11 <peyton.gardner03@gmail.com>
2023-05-09 14:28:51 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+// Function Prototypes:
+void printBoard(char[]);
+
+int main()
+{
+	const int SPOTS = 9;
+	char board[SPOTS] = {'-', '-', '-',
+						'-', '-', '-',			
+						'-', '-', '-',};
+
+	printBoard(board);
+	return 0;
+}
+
+void printBoard(char board[])
+{
+	std::cout << board[0] << " | " << board[1] << " | " << board[2] << std::endl;
+	std::cout << "----------" << std::endl;
+	std::cout << board[3] << " | " << board[4] << " | " << board[5] << std::endl;
+	std::cout << "----------" << std::endl;
+	std::cout << board[6] << " | " << board[7]<< " | " << board[8] << std::endl;
+}
