c163f7c727bef8a5cbc47309a1da5f3d45d1d3f1
peyton11 <peyton.gardner03@gmail.com>
2023-05-11 14:15:02 -0400

    add intro, add win conditions

--- main.cpp
+++ main.cpp
@@ -8,22 +8,36 @@ void printBoard(char[]);
 int playerCount();
 void player1Choice(char[]);
 void player2Choice(char[]);
-// Add player 1 win condition function
-// Add player 2 win condition function
+bool player1WinCondition(char[]);
+bool player2WinCondition(char[]);
+bool drawCondition(char[]);
 // Add AI for 1 player mode.
 // Add input validation
 // Add flow control
 
 int main()
 {
+	introduction();
+
 	const int SPOTS = 9;
 	char board[SPOTS] = {'-', '-', '-', '-', '-', '-', '-', '-', '-',};
 
-	printBoard(board);
-	player1Choice(board);
-	printBoard(board);
-	player2Choice(board);
-	printBoard(board);
+	bool player1Win = false;
+	bool player2Win = false;
+	bool draw = false;
+	
+	while (!player1Win && !player2Win && !draw)
+	{
+		player1Choice(board);
+		player1Win = player1WinCondition(board);
+		printBoard(board);
+		draw = drawCondition(board);
+
+		player2Choice(board);
+		printBoard(board);
+		player2Win = player2WinCondition(board);
+		draw = drawCondition(board);
+	}
 
 	system("pause");
 	return 0;
@@ -31,7 +45,15 @@ int main()
 
 void introduction()
 {
-	// std::cout << introduction
+	std::cout << "Welcome to Tic-Tac-Toe! In this game, Player 1 is X and player 2 is O. If you get three in a row, column, or diagonal, you win! ";
+	std::cout << "If neither player can do this, it is considered a draw! Have fun!\n\n";
+
+	std::cout << "Here is the board and the number of each space:\n";
+	std::cout << "1 | 2 | 3 " << std::endl;
+	std::cout << "----------" << std::endl;
+	std::cout << "4 | 5 | 6 " << std::endl;
+	std::cout << "----------" << std::endl;
+	std::cout << "7 | 8 | 9 " << std::endl << std::endl;
 }
 
 void printBoard(char board[])
@@ -58,8 +80,7 @@ void player1Choice(char board[])
 	std::cout << "Player 1, which space would you like to pick? ";
 	std::cin >> player1Space;
 	std::cout << std::endl;
-	player1Space -= 1;
-	board[player1Space] = 'X';
+	board[player1Space - 1] = 'X';
 }
 
 void player2Choice(char board[])
@@ -68,6 +89,110 @@ void player2Choice(char board[])
 	std::cout << "Player 2, which space would you like to pick? ";
 	std::cin >> player2Space;
 	std::cout << std::endl;
-	player2Space -= 1;
-	board[player2Space] = 'O';
+	board[player2Space - 1] = 'O';
+}
+
+bool player1WinCondition(char board[])
+{
+	bool winner = false;
+
+	if (board[0] == 'X' && board[1] == 'X' && board[2] == 'X') // Top horizontal.
+	{
+		winner = true;
+	}
+	else if (board[3] == 'X' && board[4] == 'X' && board[5] == 'X') // Middle horizontal.
+	{
+		winner = true;
+	}
+	else if (board[6] == 'X' && board[7] == 'X' && board[8] == 'X') // Bottom horizontal.
+	{
+		winner = true;
+	}
+	else if (board[0] == 'X' && board[3] == 'X' && board[6] == 'X') // Left vertical.
+	{
+		winner = true;
+	}
+	else if (board[1] == 'X' && board[4] == 'X' && board[7] == 'X') // Middle vertical.
+	{
+		winner = true;
+	}
+	else if (board[2] == 'X' && board[5] == 'X' && board[8] == 'X') // Right vertical.
+	{
+		winner = true;
+	}
+	else if (board[0] == 'X' && board[4] == 'X' && board[8] == 'X') // Diagonal down right.
+	{
+		winner = true;
+	}
+	else if (board[2] == 'X' && board[4] == 'X' && board[6] == 'X') // Diagonal down left.
+	{
+		winner = true;
+	}
+
+	// Prints out if player 1 wins.
+	if (winner)
+	{
+		std::cout << "Player 1 wins!\n";
+	}
+	return winner;
+}
+
+bool player2WinCondition(char board[])
+{
+	bool winner = false;
+
+	if (board[0] == 'O' && board[1] == 'O' && board[2] == 'O') // Top horizontal.
+	{
+		winner = true;
+	}
+	else if (board[3] == 'O' && board[4] == 'O' && board[5] == 'O') // Middle horizontal.
+	{
+		winner = true;
+	}
+	else if (board[6] == 'O' && board[7] == 'O' && board[8] == 'O') // Bottom horizontal.
+	{
+		winner = true;
+	}
+	else if (board[0] == 'O' && board[3] == 'O' && board[6] == 'O') // Left vertical.
+	{
+		winner = true;
+	}
+	else if (board[1] == 'O' && board[4] == 'O' && board[7] == 'O') // Middle vertical.
+	{
+		winner = true;
+	}
+	else if (board[2] == 'O' && board[5] == 'O' && board[8] == 'O') // Right vertical.
+	{
+		winner = true;
+	}
+	else if (board[0] == 'O' && board[4] == 'O' && board[8] == 'O') // Diagonal down right.
+	{
+		winner = true;
+	}
+	else if (board[2] == 'O' && board[4] == 'O' && board[6] == 'O') // Diagonal down left.
+	{
+		winner = true;
+	}
+
+	// Prints out if player 1 wins.
+	if (winner)
+	{
+		std::cout << "Player 2 wins!\n";
+	}
+	return winner;
+}
+
+bool drawCondition(char board[])
+{
+	bool draw = false;
+	for (int i = 0; i < 9; i++)
+	{
+		if (board[i] == '-')
+		{
+			return draw;
+		}
+	}
+	draw = true;
+	std::cout << "Draw!";
+	return draw;
 }
